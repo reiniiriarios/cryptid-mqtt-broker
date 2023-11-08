c56a1321665c50a22aa889cb3ed36ce59aa0bbfa
Emma Litwa-Vulcu <80431903+reiniiriarios@users.noreply.github.com>
2023-11-08 14:53:12 -0800

    feat: ip init logging

--- main.go
+++ main.go
@@ -11,7 +11,24 @@ import (
 	"github.com/mochi-mqtt/server/v2/listeners"
 )
 
+const MQTT_PORT = ":1883"
+
+const C_X = "\033[0m"
+const C_R = "\033[31m"
+const C_G = "\033[32m"
+const C_Y = "\033[33m"
+const C_B = "\033[34m"
+const C_P = "\033[35m"
+const C_C = "\033[36m"
+const C_W = "\033[37m"
+
 func main() {
+	println(string(C_G) + "Starting MQTT Broker...")
+	// Display local address
+	ip := GetOutboundIP()
+	println(string(C_P)+"Listening on:", ip.String()+MQTT_PORT)
+	print(string(C_X))
+
 	// Create signals channel to run server until interrupted
 	sigs := make(chan os.Signal, 1)
 	done := make(chan bool, 1)
@@ -28,7 +45,7 @@ func main() {
 	_ = server.AddHook(new(auth.AllowHook), nil)
 
 	// Create a TCP listener on a standard port.
-	tcp := listeners.NewTCP("t1", ":1883", nil)
+	tcp := listeners.NewTCP("t1", MQTT_PORT, nil)
 	err := server.AddListener(tcp)
 	if err != nil {
 		log.Fatal(err)
@@ -45,4 +62,6 @@ func main() {
 	<-done
 
 	// Cleanup
+	println()
+	println(string(C_R) + "MQTT Broker Closed" + string(C_X))
 }
