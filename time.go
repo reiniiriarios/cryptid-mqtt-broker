7334df868828dda8fab409c645324af00840af02
Emma Litwa-Vulcu <80431903+reiniiriarios@users.noreply.github.com>
2023-11-15 16:27:55 -0800

    fix: adj for local timezone

--- time.go
+++ time.go
@@ -7,11 +7,24 @@ import (
 	mqtt "github.com/mochi-mqtt/server/v2"
 )
 
+const UPDATE_TIME_INTERVAL = 60
+const TIMEZONE = "America/Los_Angeles"
+
+// Publishes a unix timestamp _in the local timezone_.
+// Assume here that this will be digested by embedded
+// systems that cannot as easily convert from UTC.
 func timePublish(s *mqtt.Server) {
-	var ts []byte
-	for range time.Tick(time.Second * 60) {
-		ts = []byte(strconv.FormatInt(time.Now().Unix(), 10))
-		err := s.Publish("current_time", ts, false, 0)
+	for range time.Tick(time.Second * UPDATE_TIME_INTERVAL) {
+		location, err := time.LoadLocation(TIMEZONE)
+		if err != nil {
+			s.Log.Error("timePublish error", err)
+			continue
+		}
+		_, tzOffset := time.Now().In(location).Zone()
+		ts := time.Now().Unix() + int64(tzOffset)
+
+		payload := []byte(strconv.FormatInt(ts, 10))
+		err = s.Publish("current_time", payload, false, 0)
 		if err != nil {
 			s.Log.Error("timePublish error", err)
 			return
